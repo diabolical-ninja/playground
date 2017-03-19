e2af5875ae3f5c23b8efe22abc8ca585e54f125d
yass <yeltahir@gmail.com>
2017-03-19 23:54:27 +1100

    Update with postgresql & odbc examples

--- R/db_con.R
+++ R/db_con.R
@@ -1,4 +1,32 @@
-# Connect to database
+#*************************************************************************************
+# Title:  Database Connections 
+# Desc:   Simple script demonstrating how to connect to a variety of databases
+# Author: Yass
+# Date:   2017-03-19
+#*************************************************************************************
+
+
+
+# Generic  ####
+# install.packages('RODBC')
+library(RODBC)
+
+
+# Setup connection
+odbc_con <- odbcDriverConnect('driver={<DB Driver>};
+                              Server=192.168.1.8;
+                              Port=5432;
+                              Database=house;
+                              Uid=<user name>;
+                              Pwd=<pw>')
+
+# Simple test query
+sqlQuery(odbc_con,'select * from leads' )
+
+
+
+
+# MySQL  ####
 
 # Load Required Packages
 install.packages('RMySQL')
@@ -7,10 +35,39 @@ library(RMySQL)
 
 # Setup connection
 drv <- dbDriver("MySQL")
-con <- dbConnect(drv, user = 'root', password = '', host = 'localhost', dbname='data')
+con <- dbConnect(drv,
+                 user = 'root',
+                 password = '', 
+                 host = 'localhost',
+                 dbname='data')
 
 
 # Simple test query
-dt <- data.table(dbGetQuery(con, "select * from data.idm_admission_source"))
+dbGetQuery(con, "select * from data.idm_admission_source")
 
 names(dt)
+
+
+
+
+
+
+# PostgreSQL  ####
+
+# install.packages('RPostgreSQL')
+library(RPostgreSQL)
+
+
+# Setup connection
+drv <- dbDriver("PostgreSQL")
+pg_con <- dbConnect(drv,
+                        host = '192.168.1.8',
+                        port = 5432,
+                        dbname = 'house',
+                        user = '<user name>',
+                        password='<pass word>')
+
+# Simple test query
+dbGetQuery(pg_con, statement = 'select * from pg_con.leads')
+
+
